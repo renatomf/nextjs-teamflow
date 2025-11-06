8dd698ed8d5dd07ba8bf7861f3b693ac10f1fa35
renatomarf76 <renatomarf76@egmail.com>
2025-11-06 17:42:47 -0300

    12: Reverse Infinite Scroll

--- app/router/message.ts
+++ app/router/message.ts
@@ -43,50 +43,73 @@ export const createMessage = base
         authorId: context.user.id,
         authorEmail: context.user.email!,
         authorName: context.user.given_name!,
-        authorAvatar: getAvatar(context.user.picture, context.user.email!)
-      }
+        authorAvatar: getAvatar(context.user.picture, context.user.email!),
+      },
     });
 
     return {
       ...created,
-    }
+    };
   });
 
-  export const listMessages = base
-    .use(requiredAuthMiddleware)
-    .use(requiredWorkspaceMiddleware)
-    .use(standardSecurityMiddleware)
-    .use(heavyWriteSecurityMiddleware)
-    .route({
-      method: "GET",
-      path: "/messages",
-      summary: "List all messages",
-      tags: ["Messages"],
-    })
-    .input(z.object({
+export const listMessages = base
+  .use(requiredAuthMiddleware)
+  .use(requiredWorkspaceMiddleware)
+  .use(standardSecurityMiddleware)
+  .use(heavyWriteSecurityMiddleware)
+  .route({
+    method: "GET",
+    path: "/messages",
+    summary: "List all messages",
+    tags: ["Messages"],
+  })
+  .input(
+    z.object({
       channelId: z.string(),
-    }))
-    .output(z.array(z.custom<Message>()))
-    .handler(async ({ input, context, errors }) => {
-      const channel = await prisma.channel.findFirst({
-        where: {
-          id: input.channelId,
-          workspaceId: context.workspace.orgCode,
-        },
-      });
-
-      if (!channel) {
-        throw errors.FORBIDDEN();
-      }
-
-      const data = await prisma.message.findMany({
-        where: {
-          channelId: input.channelId,
-        },
-        orderBy: {
-          createdAt: "desc",
-        },
-      });
-
-      return data;
+      limit: z.number().min(1).max(100).optional(),
+      cursor: z.string().optional(),
+    })
+  )
+  .output(
+    z.object({
+      items: z.array(z.custom<Message>()),
+      nextCursor: z.string().optional(),
+    })
+  )
+  .handler(async ({ input, context, errors }) => {
+    const channel = await prisma.channel.findFirst({
+      where: {
+        id: input.channelId,
+        workspaceId: context.workspace.orgCode,
+      },
     });
+
+    if (!channel) {
+      throw errors.FORBIDDEN();
+    }
+
+    const limit = input.limit ?? 30;
+
+    const messages = await prisma.message.findMany({
+      where: {
+        channelId: input.channelId,
+      },
+      ...(input.cursor
+        ? {
+            cursor: { id: input.cursor },
+            skip: 1,
+          }
+        : {}),
+
+      take: limit,
+      orderBy: [{ createdAt: "desc" }, { id: "desc" }],
+    });
+
+    const nextCursor =
+      messages.length === limit ? messages[messages.length - 1].id : undefined;
+
+    return {
+      items: messages,
+      nextCursor,
+    };
+  });
